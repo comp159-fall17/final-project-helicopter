ba690df51cbfc37dfd76f4c6018d025699d04cef
hlam1990 <h_lam4@u.pacific.edu>
2017-10-03 18:32:39 -0700

    basic vertical and horisontal movement topdown

---
+++ Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControls : MonoBehaviour {
+    public float walkSpeed = 0.1f;
+    float horizontal, vertical;
+
+    Rigidbody2D body;
+    // Use this for initialization
+    void Start () {
+        body = GetComponent<Rigidbody2D>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+        transform.Translate(horizontal * walkSpeed, vertical * walkSpeed, 0);
+    }
+
+    private void FixedUpdate()
+    {
+        
+    }
+}
