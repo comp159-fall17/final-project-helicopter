6cca527ecdbb73efa35c0b5143d613d366574246
Victor Zamarian <v_zamarian@u.pacific.edu>
2017-10-03 18:16:01 -0700

    Created very basic enemy which fires bullets at a target object. Also rotated the camera to point downwards.

---
+++ Assets/Scripts/BulletController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletController : MonoBehaviour {
+    private Rigidbody rb;
+
+    public float bulletSpeed;
+    public float lifetime;
+
+	// Use this for initialization
+	void Start() {
+        Destroy(this.gameObject, lifetime); //destroy this object after a certain amount of time
+
+        rb = gameObject.GetComponent<Rigidbody>();
+        rb.AddForce(transform.up * bulletSpeed); //shoots in the bullet's positive x direction
+	}
+	
+	// Update is called once per frame
+	void Update() {
+		
+	}
+}
---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour {
+    public GameObject bullet;
+    public float fireDelay;
+
+    public Transform target; //this could be substituted for the player's position
+
+	// Use this for initialization
+	void Start() {
+        StartCoroutine(ShootBullets());
+	}
+	
+	// Update is called once per frame
+	void Update() {
+		
+	}
+
+    private IEnumerator ShootBullets(){ //fire bullets in the direction of the target
+        while (true){
+            Vector3 targetDir = target.position - transform.position;
+            float angle = Vector3.Angle(transform.forward, targetDir);
+
+            float rotation;
+            if (transform.position.x > target.transform.position.x){
+                rotation = 90 - angle;
+            }else{
+                rotation = 90 + angle;
+            }
+
+            Instantiate(bullet, this.gameObject.transform.position, Quaternion.Euler(0, rotation, 90));
+
+            yield return new WaitForSeconds(fireDelay);
+        }
+    }
+}
