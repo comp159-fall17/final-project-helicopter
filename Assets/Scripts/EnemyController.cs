cc436a74a9be2d232337cddf05bfcc2e8bde8001
Shane Duan <s_duan@u.pacific.edu>
2017-10-05 01:34:08 -0700

    Add bonus range distance to enemies that are facing the target

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -3,12 +3,33 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyController : Shooter {
+    /// <summary>
+    /// Range of action.
+    /// </summary>
     protected float range = 10;
+
+    /// <summary>
+    /// Cone of vision (theta = visionCone * 2, centered on transform.forward).
+    /// </summary>
+    protected float visionCone = 45;
+
+    /// <summary>
+    /// How much farther the enemy can see if they're facing the target.
+    /// </summary>
+    protected float facingDistanceScale = 1.5f;
 
     protected override bool ShouldShoot {
         get {
             Vector3 target = GetTarget();
-            float targetDistance = Vector3.Distance(target, transform.position);
+            Vector3 direction = target - transform.position;
+            float targetDistance = direction.magnitude;
+
+            float angle = Vector3.Angle(direction, transform.forward);
+
+            if (Mathf.Abs(angle) < visionCone) {
+                targetDistance /= facingDistanceScale;
+            }
+
             bool inRange = targetDistance < range;
 
             if (inRange) {
