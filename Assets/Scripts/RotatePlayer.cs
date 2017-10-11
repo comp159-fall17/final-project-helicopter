9a92cf5334f93a5ad2f95763e814bb43fe990fe3
Jamie Culilap <j_culilap1@u.pacific.edu>
2017-10-11 16:23:43 -0700

    Added rotation to character

---
+++ Assets/Scripts/RotatePlayer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatePlayer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+		float moveHorizontal = Input.GetAxisRaw ("Horizontal");
+		float moveVertical = Input.GetAxisRaw ("Vertical");
+
+		Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+		transform.rotation = Quaternion.LookRotation(movement);
+	}
+}
