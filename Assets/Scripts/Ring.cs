24c59646e2b5f4c410d7aa5d227a337be23dba75
Victor Zamarian <v_zamarian@u.pacific.edu>
2017-11-28 18:37:24 -0800

    Implemented ring attack special weapon.
    Closes #5.

---
+++ Assets/Scripts/Ring.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ring : MonoBehaviour {
+
+    public float lifetime;
+    public float growthRate;
+    public float damage = 30.0f;
+
+	// Use this for initialization
+	void Start () {
+        Destroy(gameObject, lifetime);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 scale = transform.localScale;
+
+        scale.x += growthRate;
+        scale.z += growthRate;
+
+        transform.localScale = scale;
+	}
+
+    void OnTriggerEnter(Collider other) {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Player") && other.gameObject.CompareTag("Enemy")) {
+            other.gameObject.GetComponent<Shooter>().Hit(damage);
+        }
+    }
+}
---
+++ Assets/Scripts/RingWeapon.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq;
+using UnityEngine;
+
+public class RingWeapon : SpecialWeapon {
+    public GameObject ring;
+
+    void Start() {
+        Expand();
+    }
+
+    void Expand() {
+        //expand a circle around the player
+
+        Instantiate(ring, transform.position, Quaternion.Euler(0.0f, 0.0f, 0.0f));
+    }
+}
