0f74c22d9111cffa4abe2e8c93a911d202161ee6
Jake Anhaltzer <j_anhaltzer@u.pacific.edu>
2017-11-28 17:29:05 -0800

    Resized rooms and added door script

---
+++ Assets/Scripts/LevelGen/DoorScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorScript : MonoBehaviour {
+
+    // Use this for initialization
+    void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            if(this.transform.rotation.y == 0)
+            {
+                other.transform.Translate(0, 0, 5);
+            }
+            else if (this.transform.rotation.y == 1)
+            {
+                other.transform.Translate(0, 0, -5);
+            }
+            else if (this.transform.rotation.y > 0)
+            {
+                other.transform.Translate(5, 0, 0);
+            }
+            else if (this.transform.rotation.y < 0)
+            {
+                other.transform.Translate(-5, 0, 0);
+            }
+        }
+    }
+}
