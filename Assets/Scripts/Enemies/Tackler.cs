33a640bafe0b8da2b44a1579493b7ac6f66d35bb
Shane Duan <s_duan@u.pacific.edu>
2017-11-28 20:57:55 -0800

    Add wanderer enemy type
    Wanders and shoots when near player.
    
    #19 Adds new enemy type
    #21 #22 Please add new model and animation to distinguish.

--- Assets/Scripts/Enemies/Tackler.cs
+++ Assets/Scripts/Enemies/Tackler.cs
@@ -3,33 +3,12 @@ using UnityEngine;
 using UnityEngine.AI;
 
 [RequireComponent(typeof(NavMeshAgent))]
-public class Tackler : EnemyController {
-    /// <summary>
-    /// Finds next random spot to go to.
-    /// 
-    /// Adapted from https://forum.unity.com/threads/solved-random-wander-ai-using-navmesh.327950/
-    /// </summary>
-    public static Vector3 RandomNavSphere(Vector3 origin, float distance, int layermask) {
-        Vector3 randomDirection = Random.insideUnitSphere * distance + origin;
-
-        NavMeshHit navHit;
-        NavMesh.SamplePosition(randomDirection, out navHit, distance, layermask);
-        return navHit.position;
-    }
-
+public class Tackler : Wanderer {
     protected override bool ShouldShoot { get { return false; } }
 
     public float minimumDistanceToTarget = 5f;
 
     NavMeshAgent Agent;
-
-    protected override void Start() {
-        Agent = GetComponent<NavMeshAgent>();
-        Agent.speed = Speed;
-
-        // default wandering
-        StartCoroutine(Wander());
-    }
 
     protected override void Update() {
         base.Update();
@@ -38,7 +17,7 @@ public class Tackler : EnemyController {
     }
 
 #pragma warning disable RECS0135 // Function does not reach its end or a 'return' statement by any of possible execution paths
-    IEnumerator Wander() {
+    protected override IEnumerator Wander() {
 #pragma warning restore RECS0135 // Function does not reach its end or a 'return' statement by any of possible execution paths
         while (true) {
             if (hasBeenHit) {
@@ -49,7 +28,7 @@ public class Tackler : EnemyController {
             } else if (base.ShouldShoot) {
                 Agent.destination = Target;
             } else {
-                Agent.destination = RandomNavSphere(transform.position, 30, -1);
+                Agent.destination = RandomNavSphere(transform.position, 60, -1);
             }
 
             yield return new WaitUntil(() => base.ShouldShoot || Agent.remainingDistance < 0.1f);
---
+++ Assets/Scripts/Enemies/Wanderer.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Wanderer : EnemyController {
+    /// <summary>
+    /// Finds next random spot to go to.
+    /// 
+    /// Adapted from https://forum.unity.com/threads/solved-random-wander-ai-using-navmesh.327950/
+    /// </summary>
+    public static Vector3 RandomNavSphere(Vector3 origin, float distance, int layermask) {
+        Vector3 randomDirection = Random.insideUnitSphere * distance + origin;
+
+        NavMeshHit navHit;
+        NavMesh.SamplePosition(randomDirection, out navHit, distance, layermask);
+        return navHit.position;
+    }
+
+    NavMeshAgent Agent;
+
+    protected override void Start() {
+        Agent = GetComponent<NavMeshAgent>();
+        Agent.speed = Speed;
+
+        // default wandering
+        StartCoroutine(Wander());
+    }
+
+    #pragma warning disable RECS0135 // Function does not reach its end or a 'return' statement by any of possible execution paths
+    protected virtual IEnumerator Wander() {
+#pragma warning restore RECS0135 // Function does not reach its end or a 'return' statement by any of possible execution paths
+        while (true) {
+            Agent.destination = RandomNavSphere(transform.position, 30, -1);
+
+            yield return new WaitUntil(() => Agent.remainingDistance < 0.1f);
+        }
+    }
+}
