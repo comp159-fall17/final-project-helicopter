57ffd523451e86e01077b44d0d8c718c080c1024
Shane Duan <s_duan@u.pacific.edu>
2017-11-29 18:50:48 -0800

    Add basic navmesh generation to enemy room 1

---
+++ Assets/Scripts/LevelGen/NavMeshGen.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavMeshGen : MonoBehaviour {
+    static readonly List<RoomSourceTag> Rooms = new List<RoomSourceTag>();
+
+    /// <summary>
+    /// Subscribe a certain room, e.g. <code>NavMeshGen.Subscribe(this)</code>
+    /// </summary>
+    /// <param name="room">The room to subscribe.</param>
+    public static void Subscribe(RoomSourceTag room) {
+        Rooms.Add(room);
+    }
+
+    /// <summary>
+    /// Unsubscribe a certain room, e.g. <code>NavMeshGen.Unsubscribe(this)</code>
+    /// </summary>
+    /// <param name="room">The room to subscribe.</param>
+    public static void Unsubscribe(RoomSourceTag room) {
+        Rooms.Remove(room);
+    }
+
+    /// <summary>
+    /// Unsubscribe all rooms.
+    /// </summary>
+    public static void Reset() {
+        Rooms.Clear();
+    }
+
+    NavMeshData data;
+    NavMeshDataInstance instance;
+
+    void Update() {
+        GenerateNavMesh();
+    }
+
+    void OnEnable() {
+        data = new NavMeshData();
+        instance = NavMesh.AddNavMeshData(data);
+
+        GenerateNavMesh();
+    }
+
+    void OnDisable() {
+        instance.Remove();
+    }
+
+    void GenerateNavMesh() {
+        List<NavMeshBuildSource> sources = new List<NavMeshBuildSource>();
+        NavMeshBuildSettings buildSettings = NavMesh.GetSettingsByID(0);
+        Bounds localBounds = new Bounds(transform.position, new Vector3(60, 5, 60));
+
+        foreach (RoomSourceTag room in Rooms) {
+            room.Collect(ref sources);
+        }
+
+        NavMeshBuilder.UpdateNavMeshData(data, buildSettings, sources, localBounds);
+    }
+}
