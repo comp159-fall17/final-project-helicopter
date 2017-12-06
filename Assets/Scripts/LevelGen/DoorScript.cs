92c7cf809c223f33a44958bb613eeefd5601cf19
Jake Anhaltzer <j_anhaltzer@u.pacific.edu>
2017-12-05 22:38:27 -0800

    Quick fix for player rotation and doors

--- Assets/Scripts/LevelGen/DoorScript.cs
+++ Assets/Scripts/LevelGen/DoorScript.cs
@@ -4,18 +4,21 @@ using UnityEngine;
 
 public class DoorScript : MonoBehaviour {
     public static bool CanCross;
-
     public int moveDistance;
+    private float rotation;
+    private float playerRotatation;
 
     void OnTriggerEnter(Collider other) {
         if (CanCross && other.tag == "Player") {
-            if (this.transform.rotation.y == 0) {
+            other.transform.rotation = new Quaternion(0, 0, 0, 0); //Temp fix for player rotation through doors. Imporve if there is extra time.
+            rotation = this.transform.rotation.y;
+            if (rotation == 0) { //Up
                 other.transform.Translate(0, 0, moveDistance);
-            } else if (this.transform.rotation.y == 1) {
+            } else if (rotation == 1) { //Down
                 other.transform.Translate(0, 0, -moveDistance);
-            } else if (this.transform.rotation.y > 0) {
+            } else if (rotation > 0) { //Right
                 other.transform.Translate(moveDistance, 0, 0);
-            } else if (this.transform.rotation.y < 0) {
+            } else if (rotation < 0) { //Left
                 other.transform.Translate(-moveDistance, 0, 0);
             }
         }
