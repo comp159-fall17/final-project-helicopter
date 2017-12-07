18ddf79fa868f1f7a115ba5f5da0445278332d08
Shane Duan <s_duan@u.pacific.edu>
2017-12-07 14:30:01 -0800

    Add unlock new floor on death

--- Assets/Scripts/Bosses/BossController.cs
+++ Assets/Scripts/Bosses/BossController.cs
@@ -3,12 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BossController : Shooter {
+    public GameObject UnlockKey;
+
     protected override bool ShouldShoot { get { return false; } } 
     protected override bool ShouldShootSpecial { get { return false; } }
     protected override bool WallInWay { get { return false; } }
     protected override Vector3 Target { get { return transform.forward; } }
 
+    protected override void Start() {
+        UnlockKey.SetActive(false);
+    }
+
     protected override void Die() {
+        UnlockKey.SetActive(true);
         GameManager.Instance.PlayDeathSound();
         Destroy(gameObject);
     }
