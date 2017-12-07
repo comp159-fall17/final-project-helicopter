ebb483f12aad139e6bc8c5fb710f126cb586b4a3
Shane Duan <s_duan@u.pacific.edu>
2017-12-07 14:22:59 -0800

    Add Boss Controller template

---
+++ Assets/Scripts/Bosses/BossController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossController : Shooter {
+    protected override bool ShouldShoot { get { return false; } } 
+    protected override bool ShouldShootSpecial { get { return false; } }
+    protected override bool WallInWay { get { return false; } }
+    protected override Vector3 Target { get { return transform.forward; } }
+
+    protected override void Die() {
+        GameManager.Instance.PlayDeathSound();
+        Destroy(gameObject);
+    }
+}
