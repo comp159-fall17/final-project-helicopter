50704cabf232debdc0b07c0213eba85e67a1b2f0
Shane Duan <s_duan@u.pacific.edu>
2017-12-07 22:16:23 -0800

    Silence annoying Unity Chan warnings

--- Assets/Asset Store/UnityChan/Scripts/SceneLoader.cs
+++ Assets/Asset Store/UnityChan/Scripts/SceneLoader.cs
@@ -1,33 +1,31 @@
 ﻿using UnityEngine;
-using System.Collections;
 
 public class SceneLoader : MonoBehaviour {
 
-	void OnGUI()
-	{
-		GUI.Box(new Rect(10 , Screen.height - 100 ,100 ,90), "Change Scene");
-		if(GUI.Button( new Rect(20 , Screen.height - 70 ,80, 20), "Next"))
-			LoadNextScene();
-		if(GUI.Button(new Rect(20 ,  Screen.height - 40 ,80, 20), "Back"))
-			LoadPreScene();
-	}
+    void OnGUI() {
+        GUI.Box(new Rect(10, Screen.height - 100, 100, 90), "Change Scene");
+        if (GUI.Button(new Rect(20, Screen.height - 70, 80, 20), "Next"))
+            LoadNextScene();
+        if (GUI.Button(new Rect(20, Screen.height - 40, 80, 20), "Back"))
+            LoadPreScene();
+    }
 
-	void LoadPreScene()
-	{
-		int nextLevel = Application.loadedLevel + 1;
-		if( nextLevel <= 1)
-			nextLevel = Application.levelCount;
+#pragma warning disable CS0618 // Type or member is obsolete
+    void LoadPreScene() {
+        int nextLevel = Application.loadedLevel + 1;
+        if (nextLevel <= 1)
+            nextLevel = Application.levelCount;
 
-		Application.LoadLevel(nextLevel);
-	}
+        Application.LoadLevel(nextLevel);
+    }
 
-	void LoadNextScene()
-	{
-		int nextLevel = Application.loadedLevel + 1;
-		if( nextLevel >= Application.levelCount)
-			nextLevel = 1;
+    void LoadNextScene() {
+        int nextLevel = Application.loadedLevel + 1;
 
-		Application.LoadLevel(nextLevel);
+        if (nextLevel >= Application.levelCount)
+            nextLevel = 1;
 
-	}
+        Application.LoadLevel(nextLevel);
+#pragma warning restore CS0618 // Type or member is obsolete
+    }
 }
