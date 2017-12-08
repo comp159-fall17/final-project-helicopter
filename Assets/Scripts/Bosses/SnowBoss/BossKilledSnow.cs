b93853d99703a551eb563d636879d0b3aca0a6e8
Jake Anhaltzer <j_anhaltzer@u.pacific.edu>
2017-12-07 22:06:24 -0800

    Created snow boss

---
+++ Assets/Scripts/Bosses/SnowBoss/BossKilledSnow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossKilledSnow : MonoBehaviour {
+
+    public GameObject UnlockKey;
+
+    // Use this for initialization
+    void Start () {
+        UnlockKey.SetActive(false);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if(GameObject.FindGameObjectsWithTag("Enemy").Length == 0)
+        {
+            UnlockKey.SetActive(true);
+            GameManager.Instance.PlayDeathSound();
+            Destroy(GameObject.FindGameObjectWithTag("Boss"));
+        }
+    }
+}
